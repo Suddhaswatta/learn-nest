3f4a3b8c72b9b4b48d9de2e70a62b8f2da5792d4
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-10-24 01:29:14 +0530

    Catching errors and type safety using typescript

--- LEARN-TYPE/index.ts
+++ LEARN-TYPE/index.ts
@@ -2,8 +2,16 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
+interface User {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+
 axios.get(url)
-.then((res)=>{
-       console.log(res.data);
-        
-})
+    .then(res => res.data as User)
+    .then((user: User) => {
+        console.log(`User Details : \n${JSON.stringify(user, null, 4)}`);
+    })
