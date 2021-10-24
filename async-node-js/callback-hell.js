0fe04ab0d7973b7c683623458a0f28d8d2a45cad
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-10-24 17:43:40 +0530

    Pallindrome

---
+++ async-node-js/callback-hell.js
@@ -0,0 +1,54 @@
+
+console.log( "Start of main thread");
+const fetch_stdents = (callback)=>{
+    setTimeout(()=>{
+        const students = ["Adam","Eve","Madam","AAAA"]
+        console.log("Students call fetch called");
+        console.log(JSON.stringify(students));
+        callback(students)
+    },2000)
+}
+
+// Chaining 
+
+fetch_stdents((res)=>{
+    const students = res
+    cool_name(students,(name)=>{
+            console.log(`${name} is cool name !!! `);
+    })
+})
+
+
+const cool_name = (students,callback)=>{
+    for (let student of students) {
+        isPallindrome(student,(result)=>{
+            if(result==1){
+                callback(student)
+            }
+        })
+    }
+}
+
+const isPallindrome = (student,callback)=>{
+    let lo = 0
+    let hi = student.length - 1
+    while(true){
+        if(lo>=hi){
+            break;
+        }
+        if(student[lo].toLowerCase()==student[hi].toLowerCase()){
+            lo+=1
+            hi-=1
+        }
+        else{
+            break
+        }
+    }
+    if(lo>=hi){
+        callback(1)
+    }else{
+        callback(0)
+    }
+}
+
+console.log("End of main");
